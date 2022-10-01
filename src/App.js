5ab6f5dc69574b096174501c68497520ba29a44b
Manas <manasmahajan1301@gmail.com>
2022-10-01 12:51:15 +0530

    working skeleton

--- src/App.js
+++ src/App.js
@@ -1,7 +1,55 @@
 import "./App.css";
+import { AppBar, Button } from "@mui/material";
+import { useState } from "react";
+import Home from "./containers/home/Home";
+import Jobs from "./containers/jobs/Jobs";
+import Profile from "./containers/profile/Profile";
 
+const tabsList = {
+  home: "home",
+  profile: "profile",
+  jobs: "jobs",
+};
 function App() {
-  return <div className="App">Helooo</div>;
+  const [openTab, setOpenTab] = useState(tabsList.home);
+
+  const componentSwitcher = (component) => {
+    switch (component) {
+      case tabsList.home:
+        return <Home />;
+      case tabsList.jobs:
+        return <Jobs />;
+      case tabsList.profile:
+        return <Profile />;
+      default:
+        return <div>error</div>;
+    }
+  };
+  return (
+    <div className="App">
+      <AppBar position="static" className="appbar" color="transparent">
+        <div className="appBarContent">
+          <div className="appLogo">placed.pec</div>
+          <div className="appBarTabs">
+            {Object.values(tabsList).map((name, link) => {
+              return (
+                <Button
+                  className="navLink"
+                  onClick={() => {
+                    setOpenTab(name);
+                  }}
+                  variant="text"
+                >
+                  {name}
+                </Button>
+              );
+            })}
+          </div>
+        </div>
+      </AppBar>
+      <div className="contentWrapperMain">{componentSwitcher(openTab)}</div>
+    </div>
+  );
 }
 
 export default App;
