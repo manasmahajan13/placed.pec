c944241ab67f9b3757c317b7ff32d9f2b26d29df
Mohim-Singla <mohimsingla@gmail.com>
2023-02-05 15:14:29 +0530

    CGPA update from the admin portal. DRAFT-1

--- src/containers/admin/studentCG/studentCG.js
+++ src/containers/admin/studentCG/studentCG.js
@@ -1,7 +1,61 @@
-import React from 'react'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableRow,
+  TextField,
+} from "@mui/material";
+import { doc, updateDoc } from "firebase/firestore";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { fetchUsers } from "../../../api/userApi";
+import { db } from "../../../firebase-config";
 
-export default function StudentCG() {
+const StudentCG = () => {
+  const [userData, setUserData] = useState([]);
+
+  const fetchUsersData = async () => {
+    const response = await fetchUsers();
+    setUserData(response);
+  };
+
+  useEffect(() => {
+    fetchUsersData();
+  }, []);
+
   return (
-    <div>Student CGPA</div>
-  )
-}
+    <>
+      <h1>Student CGPA</h1>
+      <Table>
+        <TableBody>
+          {userData.map((user) => {
+            return (
+              <TableRow key={user.id}>
+                <TableCell>{user.SID}</TableCell>
+                <TableCell>{user.fullName}</TableCell>
+                <TableCell>
+                  <TextField
+                    size="small"
+                    onKeyDown={(event) => {
+                      // console.log(event)
+                      console.log(event);
+                      if (event.code === "Enter") {
+                        const docRef = doc(db, "users", user.id);
+                        const data = {
+                          cgpa: event.target.value
+                        };
+                        updateDoc(docRef, data)
+                      }
+                    }}
+                  />
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </>
+  );
+};
+
+export default StudentCG;
