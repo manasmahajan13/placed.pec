7e7cf2e41cf008f026e336cd59f2352445b68d4a
Mohim-Singla <mohimsingla@gmail.com>
2023-03-02 22:18:17 +0530

    Selected candidates List update feature. Draft 1

--- src/containers/admin/adminJobs/adminJobDetails/AdminJobDetails.js
+++ src/containers/admin/adminJobs/adminJobDetails/AdminJobDetails.js
@@ -5,6 +5,8 @@ import "./AdminJobDetails.css";
 import { ReactComponent as ResumeSvg } from "../../../../assets/svg/file-icon.svg";
 import { openInNewTab } from "../../../../helpers/UtilityFunctions.js";
 import {
+  Button,
+  Checkbox,
   Table,
   TableBody,
   TableCell,
@@ -13,12 +15,15 @@ import {
   TableRow,
 } from "@mui/material";
 import { HeaderTableCell } from "../../../jobs/Jobs.js";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../../../../firebase-config.js";
 
 function AdminJobDetails() {
   const { id } = useParams();
 
   const [applicants, setApplicants] = useState([]);
   const [jobDetails, setJobDetails] = useState([]);
+  const [selectedCandidateList, setSelectedCandidateList] = useState([]);
 
   const getApplicants = async () => {
     const applicantsList = await listOfusersApplied(id);
@@ -36,11 +41,33 @@ function AdminJobDetails() {
       <h1>
         {jobDetails.name} | {jobDetails.jobProfile}
       </h1>
-      <h2>Applicants</h2>
+      <div className="heading2 applicantsHeader">
+        Applicants{" "}
+        <Button
+          onClick={() => {
+            const jobDocRef = doc(db, "jobPostings", id);
+            const jobData = {
+              selectedCandidates: selectedCandidateList
+            };
+
+            updateDoc(jobDocRef, jobData)
+            selectedCandidateList.forEach((candidate) => {
+              const candidateDocRef = doc(db, "users", candidate);
+              const candidateData = {
+                selectedCompany: id
+              };
+              updateDoc(candidateDocRef, candidateData);
+            })
+          }}
+        >
+          Set as Selected
+        </Button>
+      </div>
       <TableContainer className="jobApplicantsTable">
         <Table>
           <TableHead className="applicantsHeader">
             <TableRow>
+              <HeaderTableCell> </HeaderTableCell>
               <HeaderTableCell>Name</HeaderTableCell>
               <HeaderTableCell>CGPA</HeaderTableCell>
               <HeaderTableCell>Resume</HeaderTableCell>
@@ -50,6 +77,26 @@ function AdminJobDetails() {
             {applicants.map((applicant) => {
               return (
                 <TableRow className="applicantRow" key={applicant.id}>
+                  <TableCell>
+                    <Checkbox
+                      value={selectedCandidateList.find(
+                        (c) => c === applicant.id
+                      )}
+                      onClick={(event, index) => {
+                        if (event.target.checked) {
+                          setSelectedCandidateList([
+                            ...selectedCandidateList,
+                            applicant.id,
+                          ]);
+                        } else {
+                          const filteredList = selectedCandidateList.filter(
+                            (c) => c != applicant.id
+                          );
+                          setSelectedCandidateList(filteredList);
+                        }
+                      }}
+                    />
+                  </TableCell>
                   <TableCell>{applicant.name}</TableCell>
                   <TableCell>{applicant.cgpa}</TableCell>
                   <TableCell>
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,14 +4,14 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: "AIzaSyCIlxxCWJv9UoMAq8w02TRzL51OpnBiAHY",
+  authDomain: "hired-db309.firebaseapp.com",
+  databaseURL: "https://hired-db309-default-rtdb.firebaseio.com",
+  projectId: "hired-db309",
+  storageBucket: "hired-db309.appspot.com",
+  messagingSenderId: "997596575258",
+  appId: "1:997596575258:web:1139462113a07eaa4a83a3",
+  measurementId: "G-VXXVP44TG1",
 };
 
 const app = initializeApp(firebaseConfig);
