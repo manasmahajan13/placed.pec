363839f09b436c894dc9856a88cb9a3e6912ba53
Mohim-Singla <mohimsingla@gmail.com>
2023-05-07 13:41:50 +0530

    Introduction of Placement cycle Draft-4

--- src/containers/admin/placementCycle/CreatePlacementCycle.js
+++ src/containers/admin/placementCycle/CreatePlacementCycle.js
@@ -3,11 +3,31 @@ import "./CreatePlacementCycle.css";
 import { Button, Grid, TextField } from "@mui/material";
 import { createPlacementCycle } from "../../../api/placementCycleApi";
 import { useState } from "react";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import ListItemText from "@mui/material/ListItemText";
+import Select from "@mui/material/Select";
+import Checkbox from "@mui/material/Checkbox";
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
 
 export default function CreatePlacementCycle() {
-  const [batch, setBatch] = useState("");
+  const batchItems = ["B. Tech", "M. Tech", "P.H.D."];
+  const typeItems = ["Intern", "Full Time"];
+  const [batch, setBatch] = useState([]);
   const [year, setYear] = useState("");
-  const [type, setType] = useState("");
+  const [type, setType] = useState([]);
   const [createPlacementCycleStatusCode, setCreatePlacementCycleStatusCode] =
     useState("");
 
@@ -35,17 +55,30 @@ export default function CreatePlacementCycle() {
         <div className="title">Create Placement Cycle</div>
       </div>
       <div className="newPlacementCycleBody">
-        <Grid container spacing={2}>
+        <Grid container spacing={4}>
           <Grid item xs={12}>
-            <h3>Batch</h3>
-            <TextField
-              id="Batch"
-              variant="outlined"
-              size="small"
-              onChange={(event) => {
-                setBatch(event.target.value);
-              }}
-            />
+            <FormControl sx={{ m: 1, width: 300 }}>
+              <InputLabel id="demo-multiple-checkbox-label">Batch</InputLabel>
+              <Select
+                labelId="demo-multiple-checkbox-label"
+                id="demo-multiple-checkbox"
+                multiple
+                value={batch}
+                onChange={(event) => {
+                  setBatch(event.target.value);
+                }}
+                input={<OutlinedInput label="Batch" />}
+                renderValue={(selected) => selected.join(", ")}
+                MenuProps={MenuProps}
+              >
+                {batchItems.map((batchItem) => (
+                  <MenuItem key={batchItem} value={batchItem}>
+                    <Checkbox checked={batch.indexOf(batchItem) > -1} />
+                    <ListItemText primary={batchItem} />
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
           </Grid>
           <Grid item xs={12}>
             <h3>Year</h3>
@@ -59,15 +92,28 @@ export default function CreatePlacementCycle() {
             />
           </Grid>
           <Grid item xs={12}>
-            <h3>Type</h3>
-            <TextField
-              id="type"
-              variant="outlined"
-              size="small"
-              onChange={(event) => {
-                setType(event.target.value);
-              }}
-            />
+            <FormControl sx={{ m: 1, width: 300 }}>
+              <InputLabel id="demo-multiple-checkbox-label">Type</InputLabel>
+              <Select
+                labelId="demo-multiple-checkbox-label"
+                id="demo-multiple-checkbox"
+                multiple
+                value={type}
+                onChange={(event) => {
+                  setType(event.target.value);
+                }}
+                input={<OutlinedInput label="Type" />}
+                renderValue={(selected) => selected.join(", ")}
+                MenuProps={MenuProps}
+              >
+                {typeItems.map((typeItem) => (
+                  <MenuItem key={typeItem} value={typeItem}>
+                    <Checkbox checked={batch.indexOf(typeItem) > -1} />
+                    <ListItemText primary={typeItem} />
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
           </Grid>
         </Grid>
         <br />
